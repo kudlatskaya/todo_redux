8ce3ea2765ac844846a5f77c10e94002ba734d22
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-06-01 17:35:52 +0300

    task story added

--- src/stories/assets/Task.stories.tsx
+++ src/stories/assets/Task.stories.tsx
@@ -2,6 +2,8 @@ import type {Meta, StoryObj} from '@storybook/react';
 import Task from "../../todolist/Task";
 import {TaskType} from "../../todolist/Todolist";
 import {ReduxStoreProviderDecorator} from "../decorators/ReduxStoreProviderDecorator";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "../../state/store";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof Task> = {
@@ -11,7 +13,7 @@ const meta: Meta<typeof Task> = {
     tags: ['autodocs'],
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
     args: {
-        todolistId: '321',
+        todolistId: 'todolistId1',
         task: {id: '123', title: 'JS', isDone: false} as TaskType,
     },
     decorators: [ReduxStoreProviderDecorator]
@@ -21,15 +23,11 @@ export default meta;
 type Story = StoryObj<typeof Task>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-export const AppReduxStory: Story = {
-    //render: () => <Provider store={store}><Task />  </Provider>
-};
-
+const TaskSt = () => {
+    const task = useSelector<AppRootStateType, TaskType>(state => state.tasks['todolistId1'][1])
+    return <Task task={task} todolistId={'todolistId1'}/>
+}
 export const TaskIsDone: Story = {
-    // More on args: https://storybook.js.org/docs/react/writing-stories/args
-    args: {
-        task: {id: '890', title: 'JSX', isDone: true} as TaskType,
-    },
-    //render: () => <Provider store={store}><Task />  </Provider>
+    render: () => <TaskSt />
 };
 
